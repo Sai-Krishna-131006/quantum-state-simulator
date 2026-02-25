6df7e39e5c644db200d7ae1a9f1132521c85603f
Sai Krishna Tangudu <saikrishnatangudu1310@gmail.com>
2026-02-25 06:27:01 +0530

    Added functions

--- main.py
+++ main.py
@@ -1,39 +1,76 @@
 import numpy as np
-import matplotlib .pyplot as plt 
-
-#--computational basis--#
-zero = np.array([[1], [0]])
-one = np.array([[0], [1]])
-
-#--arbitrary state--#
-psi = np.array([[3],[4]])
-
-#--normalization--#
-norm = np.sqrt(np.conjugate(psi.T)@psi)
-psi = psi / norm
-
-print('Normalized state')
-print(psi)
-
-#--projection operators--#
-P0 = zero@np.conjugate(zero.T)
-P1 = one@np.conjugate(one.T)
-
-#--born rule probabilities--#
-prob0 = (np.conjugate(psi.T)@P0@psi).real.item()
-prob1 = (np.conjugate(psi.T)@P1@psi).real.item()
-
-print('Probability of state |0> - ',prob0)
-print('Probability of state |1> - ',prob1)
-print('Sum - ',prob0+prob1)  #Should give one
+import matplotlib.pyplot as plt
 
 
+# -----------------------------
+# Basis States
+# -----------------------------
+def computational_basis():
+    zero = np.array([[1], [0]], dtype=complex)
+    one = np.array([[0], [1]], dtype=complex)
+    return zero, one
 
-states = ['|0>', '|1>']
-probs = [prob0, prob1]
 
-plt.bar(states, probs)
-plt.ylim(0,1)
-plt.title("Measurement Probabilities")
-plt.ylabel("Probability")
-plt.show()
+# -----------------------------
+# Normalize State
+# -----------------------------
+def normalize_state(psi):
+    norm = np.sqrt(np.conjugate(psi.T) @ psi)
+    return psi / norm
+
+
+# -----------------------------
+# Measurement Probabilities
+# -----------------------------
+def measurement_probabilities(psi, zero, one):
+    P0 = zero @ np.conjugate(zero.T)
+    P1 = one @ np.conjugate(one.T)
+
+    prob0 = (np.conjugate(psi.T) @ P0 @ psi).real.item()
+    prob1 = (np.conjugate(psi.T) @ P1 @ psi).real.item()
+
+    return prob0, prob1
+
+
+# -----------------------------
+# Visualization
+# -----------------------------
+def plot_probabilities(prob0, prob1):
+    states = ['|0>', '|1>']
+    probs = [prob0, prob1]
+
+    plt.bar(states, probs)
+    plt.ylim(0, 1)
+    plt.title("Measurement Probabilities")
+    plt.ylabel("Probability")
+    plt.show()
+
+
+# -----------------------------
+# Main Execution
+# -----------------------------
+def main():
+    zero, one = computational_basis()
+
+    # Create arbitrary state
+    psi = np.array([[3], [4]], dtype=complex)
+
+    # Normalize
+    psi = normalize_state(psi)
+
+    print("Normalized state:")
+    print(psi)
+
+    # Compute probabilities
+    prob0, prob1 = measurement_probabilities(psi, zero, one)
+
+    print("Probability of |0>:", prob0)
+    print("Probability of |1>:", prob1)
+    print("Sum:", prob0 + prob1)
+
+    # Plot
+    plot_probabilities(prob0, prob1)
+
+
+if __name__ == "__main__":
+    main()
